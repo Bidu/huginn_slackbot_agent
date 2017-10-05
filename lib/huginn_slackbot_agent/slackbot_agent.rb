0d68a9db8fb3a768752688d12519889e5a2d096b
Renato Menegasso <renato.menegasso@bidu.com.br>
2017-10-05 16:26:03 -0300

    Implement slack message send logical

--- lib/huginn_slackbot_agent/slackbot_agent.rb
+++ lib/huginn_slackbot_agent/slackbot_agent.rb
@@ -1,29 +1,68 @@
+require 'slack'
+
 module Agents
   class SlackbotAgent < Agent
-    default_schedule '12h'
+    gem_dependency_check { defined?(Slack) }
+
+    cannot_be_scheduled!
 
     description <<-MD
-      Add a Agent description here
+      This agent instanciate a slack bot and send messages.
+
+      Generate a slack bot token [here](https://my.slack.com/services/new/bot), and put into a credential called `slack_bot_token`
+
+      To send messages, just fill the options as a documented [here](https://api.slack.com/methods/chat.postMessage)
+
+      The bot needs to be in channel to send messages in it.
     MD
 
     def default_options
       {
+        'channel' => '',
+        'text' => '',
+        'as_user' => true,
+        'attachments' => []
       }
     end
 
     def validate_options
+      errors.add(:base, 'you need to specify the channel or user name') unless options['channel'].present?
+      errors.add(:base, 'you need to specify the text or attachments list') unless options['text'].present? || options['attachments'].any?
     end
 
     def working?
-      # Implement me! Maybe one of these next two lines would be a good fit?
-      # checked_without_error?
-      # received_event_without_error?
+      received_event_without_error?
     end
 
 #    def check
 #    end
 
-#    def receive(incoming_events)
-#    end
+    def receive(incoming_events)
+      incoming_events.each do |event|
+        interpolate_with(event) do
+          options = {
+            channel: interpolated['channel'],
+            as_user: interpolated['as_user']
+          }
+
+          options[:text] = interpolated['text'] if interpolated['text'].present?
+          options[:attachments] = interpolated['attachments'] if interpolated['attachments'].present? && interpolated['attachments'].any?
+
+          client.chat_postMessage(options)
+        end
+      end
+    end
+
+    private
+
+    def client
+      return @client if @client.present?
+
+      Slack.configure do |config|
+        config.token = credential('slack_bot_token')
+      end
+
+      @client = Slack::Web::Client.new
+    end
   end
 end
